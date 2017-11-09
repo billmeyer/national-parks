75fd229f0e1b006f678a8d91226f85ddd96e664d
echohack <echohack@users.noreply.github.com>
2017-11-08 22:15:01 -0800

    finalize GCP terraform
    Signed-off-by: echohack <echohack@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,31 +4,109 @@ provider "google" {
   region      = "${var.gcp_region}"
 }
 
-resource "google_dns_record_set" "national-parks" {
-  name = "national-parks.${google_dns_managed_zone.prod.dns_name}"
-  type = "A"
-  ttl  = 300
+////////////////////////////////
+// Firewalls
 
-  managed_zone = "${google_dns_managed_zone.prod.name}"
+resource "google_compute_firewall" "national-parks-ingress" {
+  name      = "national-parks-ingress"
+  network   = "default"
+  direction = "INGRESS"
 
-  rrdatas = ["${google_compute_instance.national-parks.network_interface.0.access_config.0.assigned_nat_ip}"]
+  allow {
+    protocol = "tcp"
+    ports    = ["8080", "9631", "9638", "27017"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["9631", "9638"]
+  }
 }
 
-resource "google_compute_target_pool" "national-parks" {
-  name      = "national-parks"
-  instances = ["${google_compute_instance.national-parks.self_link}"]
+resource "google_compute_firewall" "national-parks-egress" {
+  name      = "national-parks-egress"
+  network   = "default"
+  direction = "EGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080", "9631", "9638", "27017"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["9631", "9638"]
+  }
 }
 
-resource "google_compute_forwarding_rule" "tomcat" {
-  name       = "tomcat"
-  target     = "${google_compute_target_pool.national-parks.self_link}"
-  port_range = "8080"
+////////////////////////////////
+// Initial Peer
+
+resource "google_compute_instance" "initial-peer" {
+  name         = "initial-peer"
+  machine_type = "f1-micro"
+  zone         = "${var.gcp_zone}"
+
+  tags = ["national-parks"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-xenial-v20171011"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  count = 1
+
+  lifecycle = {
+    create_before_destroy = true
+  }
+
+  connection {
+    user        = "${var.gcp_image_user}"
+    private_key = "${file("${var.gcp_private_key}")}"
+  }
+
+  provisioner "file" {
+    content     = "${data.template_file.install_hab.rendered}"
+    destination = "/tmp/install_hab.sh"
+  }
+
+  provisioner "file" {
+    content     = "${data.template_file.initial_peer.rendered}"
+    destination = "/home/echohack/hab-sup.service"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo adduser --group hab",
+      "sudo useradd -g hab hab",
+      "chmod +x /tmp/install_hab.sh",
+      "sudo /tmp/install_hab.sh",
+      "sudo mv /home/echohack/hab-sup.service /etc/systemd/system/hab-sup.service",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl start hab-sup",
+      "sudo systemctl enable hab-sup",
+    ]
+  }
 }
+
+////////////////////////////////
+// Instances
 
 resource "google_compute_instance" "np-mongodb" {
   name         = "np-mongodb"
   machine_type = "n1-standard-1"
   zone         = "${var.gcp_zone}"
+
+  tags = ["national-parks"]
 
   boot_disk {
     initialize_params {
@@ -80,7 +158,7 @@ resource "google_compute_instance" "np-mongodb" {
       "sudo systemctl start hab-sup",
       "sudo systemctl enable hab-sup",
       "sudo hab pkg install /home/echohack/echohack-np-mongodb-3.2.9-20171026032947-x86_64-linux.hart",
-      "sudo hab start echohack/np-mongodb --strategy at-once",
+      "sudo hab svc load echohack/np-mongodb --group prod --strategy at-once",
     ]
   }
 }
@@ -89,6 +167,8 @@ resource "google_compute_instance" "national-parks" {
   name         = "national-parks"
   machine_type = "n1-standard-1"
   zone         = "${var.gcp_zone}"
+
+  tags = ["national-parks"]
 
   boot_disk {
     initialize_params {
@@ -126,7 +206,7 @@ resource "google_compute_instance" "national-parks" {
   }
 
   provisioner "local-exec" {
-    command = "scp -oStrictHostKeyChecking=no -i ${var.gcp_private_key} /Users/echohack/code/national-parks/results/echohack-national-parks-5.7.0-20171026050054-x86_64-linux.hart ${var.gcp_image_user}@${self.network_interface.0.access_config.0.assigned_nat_ip}:/home/echohack"
+    command = "scp -oStrictHostKeyChecking=no -i ${var.gcp_private_key} /Users/echohack/code/national-parks/results/echohack-national-parks-5.6.0-20171026043832-x86_64-linux.hart ${var.gcp_image_user}@${self.network_interface.0.access_config.0.assigned_nat_ip}:/home/echohack"
   }
 
   provisioner "remote-exec" {
@@ -139,24 +219,28 @@ resource "google_compute_instance" "national-parks" {
       "sudo systemctl daemon-reload",
       "sudo systemctl start hab-sup",
       "sudo systemctl enable hab-sup",
-      "sudo hab pkg install /home/echohack/echohack-national-parks-5.7.0-20171026050054-x86_64-linux.hart",
-      "sudo hab start echohack/national-parks --peer ${google_compute_instance.np-mongodb.network_interface.0.address} --bind database:np-mongodb.default --strategy at-once",
+      "sudo hab pkg install /home/echohack/echohack-national-parks-5.6.0-20171026043832-x86_64-linux.hart",
+      "sudo hab svc load echohack/national-parks --group prod --bind database:np-mongodb.prod --strategy at-once",
     ]
   }
-  depends_on = ["google_compute_instance.np-mongodb"]
 }
 
-resource "google_dns_managed_zone" "prod" {
-  name     = "prod-zone"
-  dns_name = "habitat-kubernetes-playland.com."
-}
-
+////////////////////////////////
 // Templates
+
+data "template_file" "initial_peer" {
+  template = "${file("${path.module}/templates/hab-sup.service")}"
+
+  vars {
+    flags = "--auto-update --listen-gossip 0.0.0.0:9638 --listen-http 0.0.0.0:9631 --permanent-peer"
+  }
+}
+
 data "template_file" "sup_service" {
   template = "${file("${path.module}/templates/hab-sup.service")}"
 
   vars {
-    flags = "--auto-update"
+    flags = "--auto-update --peer ${google_compute_instance.initial-peer.network_interface.0.address} --listen-gossip 0.0.0.0:9638 --listen-http 0.0.0.0:9631"
   }
 }
 
